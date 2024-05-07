5cec551b92d7010f3e96173eec1056476ac33d3a
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-05-08 00:56:13 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,12 @@
+const getquotes = async () => {
+    try{
+        const res = await fetch("https://api.quotable.io/random");
+        const data = await res.json();
+        const myquote = document.querySelector("#myquote");
+        myquote.innerHTML = data.content;
+    } catch (error) {}
+};
+
+window.addEventListener("load", () => {
+    getquotes();
+});
