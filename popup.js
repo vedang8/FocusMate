c0e34a8a21b2fef91c24bb42750dd6e30d662670
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-05-08 10:34:06 +0530

    loader is implemented

--- popup.js
+++ popup.js
@@ -12,3 +12,25 @@ const getquotes = async () => {
 window.addEventListener("load", () => {
     getquotes();
 });
+
+
+document.getElementById("start-session-btn").addEventListener("click", function(){
+    showLoader(); // show the loader when the button is clicked
+    showOverlay();
+    setTimeout(redirect, 3000); // start work session after 3 sec
+});
+
+// function to show the loader
+function showLoader(){
+    const loader = document.getElementById("loader");
+    loader.style.display = "block";
+}
+// Function to show the overlay
+function showOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'block';
+}
+
+function redirect(){
+   window.location.href = "working_session.html";
+}
