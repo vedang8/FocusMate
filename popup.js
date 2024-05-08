373bc553b53e6a4c70799b0406be454f966b15f5
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-05-08 17:26:04 +0530

    sliding navigation bar is implemented

--- popup.js
+++ popup.js
@@ -13,24 +13,8 @@ window.addEventListener("load", () => {
     getquotes();
 });
 
-
-document.getElementById("start-session-btn").addEventListener("click", function(){
-    showLoader(); // show the loader when the button is clicked
-    showOverlay();
-    setTimeout(redirect, 3000); // start work session after 3 sec
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+btn.addEventListener("click", function(){
+    sidebar.classList.toggle('active');
 });
-
-// function to show the loader
-function showLoader(){
-    const loader = document.getElementById("loader");
-    loader.style.display = "block";
-}
-// Function to show the overlay
-function showOverlay() {
-    const overlay = document.getElementById('overlay');
-    overlay.style.display = 'block';
-}
-
-function redirect(){
-   window.location.href = "working_session.html";
-}
