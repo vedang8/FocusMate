fb972a656fe90f8d4c86a33ea15895a419c6c363
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-05-13 17:31:56 +0530

    pomodoro timer implemented

--- Focus Mate/JS/pomodoro.js
+++ Focus Mate/JS/pomodoro.js
@@ -21,23 +21,65 @@ function formatNumberInStringMinutes(number){
     return `${minutes}:${seconds}`;
 }
 
+
+/* start timer */
+document.getElementById("start-btn").addEventListener("click", function(){
+    startTimer();
+});
+
 const startTimer = () =>{
-    console.log("startt");
+    console.log("start");
     progressInterval = setInterval(()=>{
         timerValue--;
         setInfoCircularProgressBar();
     }, 1000);
 }
 
+document.getElementById("stop-btn").addEventListener("click", function(){
+    stopTimer();
+});
 const stopTimer = () => clearInterval(progressInterval);
+
+document.getElementById("reset-btn").addEventListener("click", function(){
+    resetTimer();
+});
+const resetTimer = () => {
+    clearInterval(progressInterval);
+    timerValue = (pomodoroType === TIMER_TYPE_POMODORO) ? pomodoroTimerInSeconds : shortBreakTimerInSeconds;
+    
+    multiplierFactor = 360 / timerValue;
+    setInfoCircularProgressBar();
+    audio.stop();
+}
 
 function setInfoCircularProgressBar(){
     if(timerValue === 0){
         stopTimer();
         audio.play();
     }
-
+    
     circularProgressBarNumber.textContent = `${formatNumberInStringMinutes(timerValue)}`;
 
-    //circularProgressBar.style.background = `conic-gradient( ${timerValue = multiplierFactor } 360deg, purple 0deg)`
+    circularProgressBar.style.background = `conic-gradient( ${timerValue * multiplierFactor } 360deg, purple 0deg)`;
 }
+
+document.getElementById("buttonTypePomodoro").addEventListener("click", function(){
+    setPomodoroType(TIMER_TYPE_POMODORO);
+});
+
+document.getElementById("buttonTypeShortBreak").addEventListener("click", function(){
+    setPomodoroType(TIMER_TYPE_SHORT_BREAK);
+});
+
+const setPomodoroType = (type) => {
+    pomodoroType = type;
+    if(type === TIMER_TYPE_POMODORO){
+       buttonTypeShortBreak.classList.remove("active"); 
+       buttonTypePomodoro.classList.add("active"); 
+    }else{
+        buttonTypePomodoro.classList.remove("active");
+        buttonTypeShortBreak.classList.add("active");
+    }
+    resetTimer();
+}
+ 
